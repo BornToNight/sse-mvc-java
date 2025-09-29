96bfd4d8d13d67695ca6467dcb0c22d66dae50a6
Vadim <isako-vadim@yandex.ru>
2025-09-29 16:59:44 +0500

    REFACTOR

--- src/main/java/ru/pachan/sse_mvc_java/SseMvcApplication.java
+++ src/main/java/ru/pachan/sse_mvc_java/SseMvcApplication.java
@@ -7,6 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class SseMvcApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(SseMvcApplication.class, args);
     }
